bed98020f151957e54b1f9c35ca5a16cf0705a9f
ihteshamm123 <39794328+ihteshamm123@users.noreply.github.com>
2020-06-04 03:15:40 -0700

    Update
    Adding FSM.

---
+++ FPS-Project/Assets/Gun/Scripts/GunPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPlayer : MonoBehaviour
+{
+    public Camera PlayerCamera;
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Fire!");
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            bulletObject.transform.position = PlayerCamera.transform.position
+                + PlayerCamera.transform.forward;
+            bulletObject.transform.forward = PlayerCamera.transform.forward;
+        }
+    }
+}
---
+++ FPS-Project/Assets/followPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class followPlayer : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent agent = null;
+    public Transform destination = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > 20)
+        {
+            agent.SetDestination(destination.position);
+        }
+    }
+
+}
